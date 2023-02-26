0f14258fd85e47572371c8a0289284103ca43802
PSNYAutomation <psny.automation@gmail.com>
2023-02-26 12:24:35 -0500

    Initial Commit

---
+++ JavaFeb23/src/dataTypes/If_Demo.java
@@ -0,0 +1,36 @@
+package dataTypes;
+
+public class If_Demo {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Syntax: 
+		 *  if(boolean condition)
+		 * {
+		 * Code to be executed 
+		 * }
+		 * 
+		 */
+		
+		int age = 16; 
+		System.out.println("Testing if condintion starts: ");
+		
+		if (age>18) 
+		{
+			System.out.println("You are eligible to marry !!");
+		}
+		
+		System.out.println("Testing if condintion ends !!!");
+
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
